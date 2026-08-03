352f67fe676717e175c10af6a087a7e4dab531d2
denixl-11 <stepusindenis@gmail.com>
2026-08-03 14:10:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ AUTH_CACHE_TTL = 300
 AUTH_CACHE_MAX = 2048
 RESTRICTION_CACHE_TTL = 3
 RESTRICTION_CACHE_MAX = 4096
-API_RELEASE = "8.9-opt.71"
+API_RELEASE = "8.9-opt.72"
 GITHUB_CASE_ASSET_BASE = os.getenv(
     "GITHUB_CASE_ASSET_BASE",
     "https://denixl-11.github.io/dnx-store/assets/case-rewards/",
